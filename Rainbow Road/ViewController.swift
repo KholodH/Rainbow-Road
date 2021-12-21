230f8c17711c2ec1e3921f1ebb5610bf0c3d6c4c
kholod <kholodhalmalki@gmail.com>
2021-12-21 20:00:37 +0000

    up

--- Rainbow Road/ViewController.swift
+++ Rainbow Road/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    let colors = [UIColor.red, UIColor.orange, UIColor.yellow ,UIColor.green , UIColor.blue, UIColor.purple]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
 
-
+        func tableView(_ taskListTable: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 6
+        }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200.0;//Choose your custom row height
+    }
+        func tableView(_ taskListTable: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = taskListTable.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            
+            cell.contentView.backgroundColor = colors[indexPath.row]
+            return cell
+        }
 }
 
